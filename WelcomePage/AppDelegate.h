4a5e094b9441f0a0374c97ba15c7a5d1ea280e2e
ran354101066 <wangran354101066@163.com>
2015-08-04 13:59:08 +0800

    Initial Commit

---
+++ WelcomePage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WelcomePage
+//
+//  Created by wr on 15/8/4.
+//  Copyright (c) 2015年 WR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WelcomePage/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WelcomePage
+//
+//  Created by wr on 15/8/4.
+//  Copyright (c) 2015年 WR. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
